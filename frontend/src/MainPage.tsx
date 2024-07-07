1ffe4689fa3532e7782593dd6a2d858b98036369
OHubert90 <166997763+OHubert90@users.noreply.github.com>
2024-07-07 19:20:38 +0200

    update icon url

--- frontend/src/MainPage.tsx
+++ frontend/src/MainPage.tsx
@@ -80,7 +80,7 @@ const MainPage: React.FC = () => {
         };
 
         try {
-            const response = await axios.post('${baseUrl}/api/messages/send', message);
+            const response = await axios.post('/api/messages/send', message);
             const savedMessage = response.data;
 
             setMessages((prevMessages) => [
@@ -123,7 +123,7 @@ const MainPage: React.FC = () => {
                     {user && (
                         <>
                             <h2>Welcome</h2>
-                            <img src={`${baseUrl}/icons/${user.userIcon}`} alt="User Icon" className="user-icon" />
+                            <img src={`/icons/${user.userIcon}`} alt="User Icon" className="user-icon" />
                             <span>{user.username}</span>
                         </>
                     )}
